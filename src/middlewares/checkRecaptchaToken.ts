ad1bc06bc9ddf2ca080e72dc053437aacaabe276
mstat777 <statevd@gmail.com>
2024-11-04 13:45:59 +0100

    Minor fix

--- src/middlewares/checkRecaptchaToken.ts
+++ src/middlewares/checkRecaptchaToken.ts
@@ -2,11 +2,16 @@ import { Request, Response, NextFunction } from 'express';
 import axios from 'axios';
 
 export const checkRecaptchaToken: any = async (req: Request, res: Response, next: NextFunction) => {
-    const { token } = req.body;
+    const { captcha } = req.body;
 
+    if (!captcha) {
+        return res.status(400).json({msg: 'Please complete the CAPTCHA'});
+    }
+
+    console.log(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.GOOGLE_SK}&response=${captcha}`);
     try {
         const result = await axios.post(
-            `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.GOOGLE_SK}&response=${token}`,
+            `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.GOOGLE_SK}&response=${captcha}`,
             {
                 headers: {
                     'Access-Control-Allow-Origin': 'https://dimitarstatev.com',
@@ -18,13 +23,13 @@ export const checkRecaptchaToken: any = async (req: Request, res: Response, next
         
         if (!result.data.success) {
             console.log("ReCAPTCHA token is NOT valid!!!");
-            return res.status(403).json({msg: "Robot 🤖!!!"});
+            return res.status(403).json({msg: "ReCAPTCHA verification failed"});
         } else {
             console.log("ReCAPTCHA token is valid.");
             next();
         }
     } catch (error) {
         console.error(error);
-        res.status(500).json({msg: "Error verifying reCAPTCHA"});
+        res.status(500).json({msg: "Error verifying ReCAPTCHA"});
     }
 }

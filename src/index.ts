c1f952cb3c2490641b945051060d45ab451b3ef9
mstat777 <statevd@gmail.com>
2024-11-04 15:24:09 +0100

    Minor fix

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,15 @@ dotenv.config();
 const app: Express = express();
 const PORT = process.env.LOCAL_PORT;
 
-var whitelist = ['https://dimitarstatev.com', 'http://127.0.0.1:3000']
-var corsOptions = {
-  origin: function (origin: any, callback: any) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
+var whitelist = ['https://dimitarstatev.com', 'http://127.0.0.1:3000'];
+var corsOptions = function (req: any, callback: any) {
+    var corsOptions;
+    if (whitelist.indexOf(req.header('Origin')) !== -1) {
+        corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+    }else{
+        corsOptions = { origin: false } // disable CORS for this request
     }
-  }
+    callback(null, corsOptions) // callback expects two parameters: error and options
 }
 
 app.use(cors(corsOptions))
